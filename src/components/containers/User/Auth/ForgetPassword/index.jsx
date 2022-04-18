e5060c1b08f70a85ca211d5ab490d8ca9c78e67d
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-18 16:41:06 +0700

    Update ForgetPassword

--- src/components/containers/User/Auth/ForgetPassword/index.jsx
+++ src/components/containers/User/Auth/ForgetPassword/index.jsx
@@ -4,16 +4,32 @@ import * as SC from "./../../../styledComponents";
 import { AiOutlineMail } from "react-icons/ai";
 import { Input } from "antd";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
 export function ForgetPassword() {
   const { t } = useTranslation();
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: ''
+    }
+  });
+  const onSubmit = data => console.log(data);
   return (
-    <div className="forgetPasswordContainer">
+    <div className={style.forgetPasswordContainer}>
+      <SC.Typo fsize="2rem" fw="800" transform="uppercase" pd="1rem 0">
+        {t("reset-password")}
+      </SC.Typo>
       <SC.Typo>{t("enter-email")}</SC.Typo>
-      <Input
-        size="large"
-        placeholder="Email"
-        prefix={<AiOutlineMail className={style.email} />}
-      />
+      <form onSubmit={handleSubmit(onSubmit)>
+        <div className={style.InputContainer}>
+          <Input
+            size="large"
+            placeholder="Email"
+            prefix={<AiOutlineMail className={style.email} />}
+          />
+
+          <SC.Button>{t("send")}</SC.Button>
+        </div>
+      </form>
     </div>
   );
 }
