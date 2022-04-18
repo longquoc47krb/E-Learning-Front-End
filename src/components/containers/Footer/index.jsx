444a3063969b46c85ec742d1974bed45f9f24061
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-18 16:41:25 +0700

    Update Footer

--- src/components/containers/Footer/index.jsx
+++ src/components/containers/Footer/index.jsx
@@ -1,67 +1,36 @@
 import React from "react";
-
 export const Footer = () => {
   return (
-    <div className="footer-dark">
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6 col-md-3 item">
-              <h3>Services</h3>
-              <ul>
-                <li>
-                  <a href="#">Web design</a>
-                </li>
-                <li>
-                  <a href="#">Development</a>
-                </li>
-                ss
-                <li>
-                  <a href="#">Hosting</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-sm-6 col-md-3 item">
-              <h3>About</h3>
-              <ul>
-                <li>
-                  <a href="#">Company</a>
-                </li>
-                <li>
-                  <a href="#">Team</a>
-                </li>
-                <li>
-                  <a href="#">Careers</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-6 item text">
-              <h3>Lodemy</h3>
-              <p>
-                Praesent sed lobortis mi. Suspendisse vel placerat ligula.
-                Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis
-                tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel
-                in justo.
-              </p>
-            </div>
-            <div className="col item social">
-              <a href="#">
-                <i className="icon ion-social-facebook"></i>
-              </a>
-              <a href="#">
-                <i className="icon ion-social-twitter"></i>
-              </a>
-              <a href="#">
-                <i className="icon ion-social-snapchat"></i>
-              </a>
-              <a href="#">
-                <i className="icon ion-social-instagram"></i>
-              </a>
-            </div>
-          </div>
-          <p className="copyright">Lodemy © 2022</p>
-        </div>
-      </footer>
-    </div>
+    <footer class="relative w-full bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 p">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022{" "}
+        <a href="/" class="hover:underline">
+          Lodemy™
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="#" class="mr-4 hover:underline md:mr-6">
+            Licensing
+          </a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 };
