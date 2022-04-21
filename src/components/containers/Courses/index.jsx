19edc71d53fb4431dab515b3fcda7b3346f75187
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-21 20:52:17 +0700

    Update course white pagination

--- src/components/containers/Courses/index.jsx
+++ src/components/containers/Courses/index.jsx
@@ -5,18 +5,9 @@ import { t } from "i18next";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import style from "./Course.module.scss";
-import CourseItem from "./CourseItem";
-import { Loading, Pagination } from "./../../presentational";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardImgOverlay,
-} from "reactstrap";
-
+import { Loading } from "./../../presentational";
+import UsePagination from "../../presentational/Pagination/usePagination";
+import { Pagination } from "@mui/material/Pagination";
 export const CoursesContainer = styled.div`
   padding: 0 1rem;
   display: ${(props) => (props.display ? props.display : "static")};
@@ -30,38 +21,34 @@ export const CoursesGrid = styled.div`
 `;
 export function Courses() {
   const { t } = useTranslation();
-  const [courses, setCourse] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [coursesPerPage] = useState(6);
-
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const PerPage = 4;
+  const firstIndex = page * PerPage;
+  const lastIndex = page * PerPage + PerPage;
   useEffect(() => {
-    setLoading(true);
-    const getImages = async (keyword, count) => {
-      let client_id = "vMnXHdmz6WhzBIiL-R9zfyNfWqE9Nxpo1v-WBiAuUxU";
-      const res = await axios
-        .get(
-          `https://api.unsplash.com/search/photos?query=${keyword}&per_page=${count}&orientation=landscape&page=1&client_id=${client_id}`
-        )
-        .then((res) => {
-          const { results } = res.data;
-          setLoading(false);
-          setCourse(results);
-        });
-    };
-
-    getImages("football", 10);
-    console.log(courses);
+    (async () => {
+      try {
+        const data = await axios.get(
+          "https://random-data-api.com/api/nation/random_nation?size=50"
+        );
+        setPeople(data.data);
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    })();
   }, []);
-  // Get current Courses
-  const indexOfLastCourse = currentPage * coursesPerPage;
-  const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
-  const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
 
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const handleChangePage = (event, newPage) => {
+    // set current page as newPage
+    setPage(newPage - 1);
+  };
   return (
-    <CoursesContainer>
+    <div>
       <SC.Typo
         fsize="3rem"
         fw="700"
@@ -73,26 +60,45 @@ export function Courses() {
       >
         {t("top-courses")}
       </SC.Typo>
-      <CoursesGrid>
-        {loading ? <Loading /> : <div></div>}
-        {courses.map((course) => {
-          <Card className={style.card}>
-            <div className={style.cardImg}>
-              <CardImg top width="100%" src={course.urls.full} />
+      <div>
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <h2>Error fetching users</h2>
+        ) : (
+          <>
+            <div className="content">
+              <div className="items">
+                {people.slice(firstIndex, lastIndex).map((el) => (
+                  <div className="item" key={el.uid}>
+                    <img
+                      src={`https://avatars.dicebear.com/api/miniavs/${el.first_name}.svg`}
+                      alt={`${el.username} profile`}
+                      className="item__img"
+                    />
+
+                    <div className="item__info">
+                      <p className="nationality">
+                        Nationality: {el.nationality}
+                      </p>
+                      <p className="lang">Language: {el.language}</p>
+                      <p className="capital">{el.capital}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="pagination">
+                <UsePagination
+                  pageCount={Math.ceil(people.length / 4)}
+                  onChangeHandler={handleChangePage}
+                  variant={"text"}
+                  className={style.paginationLinks}
+                />
+              </div>
             </div>
-            <CardBody>
-              <CardTitle>
-                <SC.Typo transform="capitalize">{course.description}</SC.Typo>
-              </CardTitle>
-            </CardBody>
-          </Card>;
-        })}
-      </CoursesGrid>
-      <Pagination
-        postsPerPage={coursesPerPage}
-        totalPosts={courses.length}
-        paginate={paginate}
-      />
-    </CoursesContainer>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
