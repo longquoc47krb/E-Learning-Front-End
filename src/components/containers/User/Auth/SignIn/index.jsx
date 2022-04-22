447be6a46b3277edbb0ea32d010629698929f680
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-22 17:17:04 +0700

    Update SignIn

--- src/components/containers/User/Auth/SignIn/index.jsx
+++ src/components/containers/User/Auth/SignIn/index.jsx
@@ -14,7 +14,8 @@ import { Input } from "antd";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
-
+import { Paper } from "@mui/material";
+import { FaFacebookF, FaGoogle } from "react-icons/fa";
 export function SignIn() {
   const navigate = useNavigate();
   const { t } = useTranslation();
@@ -26,16 +27,18 @@ export function SignIn() {
   });
   return (
     <div className={style.signinContainer}>
-      <div className={style.signinLeft}>
+      <Paper elevation={16} className={style.signinForm}>
         <SC.Typo fsize="2rem" fw="800" transform="uppercase" pd="1rem 0">
           {t("signin")}
         </SC.Typo>
         <div className={style.InputContainer}>
+          <label>Email</label>
           <Input
             size="large"
             placeholder="Email"
             prefix={<AiOutlineMail className={style.email} />}
           />
+          <label>{t("password")}</label>
           <Input.Password
             size="large"
             placeholder="Password"
@@ -46,34 +49,32 @@ export function SignIn() {
           />
         </div>
         <SC.Typo
-          mg="0 20% 1rem 0"
           className={style.forget}
           onClick={() => navigate("/auth/forget-password")}
         >
           {t("forget-pw")}
         </SC.Typo>
-        <SC.Button type="submit" width="60%" mg="0 0 2rem 0">
+        <SC.Button type="submit" width="70%" mg="0 0 2rem 0">
           {t("signin")}
         </SC.Button>
         <SC.Typo fw="400" mg="0 0 10px 0" className={style.middleLine}>
           {t("sign-in-with-social")}
         </SC.Typo>
         <div className={style.socialContainer}>
-          <img src={Facebook} alt="" id="fb" />
-          <img src={Google} alt="" id="gg" />
+          <div className="social-share-item facebook">
+            <FaFacebookF className="social-share-icon" />
+            <span className="social-share-text">{t("login-fb")}</span>
+          </div>
+          <div className="social-share-item instagram">
+            <FaGoogle className="social-share-icon" />
+            <span className="social-share-text">{t("login-gg")}</span>
+          </div>
         </div>
         <div className={style.goToSignUp}>
           <SC.Typo>{t("have-yet-account")}</SC.Typo>
           <Link to="/auth/signup">{t("signup")}</Link>
         </div>
-      </div>
-      <div className={style.signinRight}>
-        <img
-          src="https://images.unsplash.com/photo-1550592704-6c76defa9985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          alt=""
-          className="src"
-        />
-      </div>
+      </Paper>
     </div>
   );
 }
