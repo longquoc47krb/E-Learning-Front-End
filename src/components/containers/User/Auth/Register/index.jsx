f6dba0adb60c82f974bba398da684f3c5555e732
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-22 17:11:27 +0700

    Register ( chỉnh layout )

--- src/components/containers/User/Auth/Register/index.jsx
+++ src/components/containers/User/Auth/Register/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import style from "./../Auth.module.scss";
 import * as SC from "./../../../styledComponents";
-import Facebook from "./../../../../../assets/facebook.svg";
-import Google from "./../../../../../assets/google.svg";
+import { Paper } from "@mui/material";
 import {
   AiOutlineMail,
   AiOutlineLock,
@@ -15,47 +14,53 @@ import { Link } from "react-router-dom";
 export function Register() {
   const { t } = useTranslation();
   return (
-    <div class={style.signupContainer}>
-      <form className={style.signUpform} action="#">
-        <SC.Typo fsize="2rem" fw="800" transform="uppercase" mg="0 0 2rem 0">
-          {t("create-account")}
-        </SC.Typo>
-        <div className={style.InputContainer} id="signin">
-          <Input
-            size="large"
-            placeholder={t("full-name")}
-            prefix={<AiOutlineMail className={style.fullName} />}
-          />
-          <Input
-            size="large"
-            placeholder="Email"
-            prefix={<AiOutlineMail className={style.email} />}
-          />
-          <Input.Password
-            size="large"
-            placeholder={t("password")}
-            prefix={<AiOutlineLock className={style.password} />}
-            iconRender={(visible) =>
-              visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />
-            }
-          />
-          <Input.Password
-            size="large"
-            placeholder={t("re-type-password")}
-            prefix={<AiOutlineLock className={style.repassword} />}
-            iconRender={(visible) =>
-              visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />
-            }
-          />
-        </div>
-        <SC.Button mg="0 0 1rem 0" width="50%">
-          {t("signup")}
-        </SC.Button>
-        <div className={style.goToSignIn}>
-          <SC.Typo>{t("already-account")}</SC.Typo>
-          <Link to="/auth/signin">{t("signin")}</Link>
-        </div>
-      </form>
+    <div className={style.signupContainer}>
+      <Paper className={style.signUpform} elevation={16}>
+        <form action="#">
+          <SC.Typo fsize="2rem" fw="800" transform="uppercase" mg="0 0 2rem 0">
+            {t("create-account")}
+          </SC.Typo>
+          <div className={style.InputContainer} id="signin">
+            <label>{t("full-name")}</label>
+            <Input
+              size="large"
+              placeholder={t("full-name")}
+              prefix={<AiOutlineMail className={style.fullName} />}
+            />
+            <label>{t("Email")}</label>
+            <Input
+              size="large"
+              placeholder="Email"
+              prefix={<AiOutlineMail className={style.email} />}
+            />
+            <label>{t("password")}</label>
+            <Input.Password
+              size="large"
+              placeholder={t("password")}
+              prefix={<AiOutlineLock className={style.password} />}
+              iconRender={(visible) =>
+                visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />
+              }
+            />
+            <label>{t("re-type-password")}</label>
+            <Input.Password
+              size="large"
+              placeholder={t("re-type-password")}
+              prefix={<AiOutlineLock className={style.repassword} />}
+              iconRender={(visible) =>
+                visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />
+              }
+            />
+          </div>
+          <SC.Button mg="1rem 0 1rem 0" width="50%">
+            {t("signup")}
+          </SC.Button>
+          <div className={style.goToSignIn}>
+            <SC.Typo>{t("already-account")}</SC.Typo>
+            <Link to="/auth/signin">{t("signin")}</Link>
+          </div>
+        </form>
+      </Paper>
     </div>
   );
 }
