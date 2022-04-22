2a07260ec8facc3fe4c8b5784e53d37edb399b6e
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-04-22 17:10:12 +0700

    Search ( chưa fix nhưng push tạm :v )

--- src/components/containers/Search/index.jsx
+++ src/components/containers/Search/index.jsx
@@ -1,15 +1,58 @@
-import { Input } from "antd";
-import React from "react";
+import { AutoComplete } from "antd";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
 export const Search = () => {
-  const { Search } = Input;
+  const [value, setValue] = useState("");
+  // const [options, setOptions] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const onChange = (data) => {
+    setValue(data);
+  };
+  const onSelect = (data) => {
+    console.log("onSelect", data);
+  };
+  const onSearch = (searchText) => {
+    // setOptions(!searchText ? [] : options);
+  };
+  const options = [
+    {
+      name: "long",
+    },
+    {
+      name: "quoc",
+    },
+  ];
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         "https://provinces.open-api.vn/api/?depth=2"
+  //       );
+  //       setOptions(res.data);
+  //       console.log(options);
+  //     } catch {
+  //       setError(true);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   })();
+  // }, []);
   const { t } = useTranslation();
-  const onSearch = (value) => console.log(value);
   return (
-    <Search
-      placeholder={t("search-placeholder")}
-      onSearch={onSearch}
-      style={{ width: "auto" }}
-    />
+    <>
+      <AutoComplete
+        style={{
+          width: 300,
+        }}
+        options={options}
+        onSelect={onSelect}
+        placeholder="try to type `b`"
+        filterOption={(inputValue, option) =>
+          option.name.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+      />
+    </>
   );
 };
