9162f3ccdaad0b53ad9039f8e7c968ec4c44de18
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2022-05-05 18:19:18 +0700

    Update Register ( thêm firebase)

--- src/components/containers/User/Auth/Register/index.jsx
+++ src/components/containers/User/Auth/Register/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import style from "./../Auth.module.scss";
 import * as SC from "./../../../styledComponents";
 import { Paper } from "@mui/material";
@@ -10,10 +10,35 @@ import {
 } from "react-icons/ai";
 import { useTranslation } from "react-i18next";
 import { Input } from "antd";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../../../../context/AuthContext";
 export function Register() {
   const { t } = useTranslation();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
   return (
     <div className={style.signupContainer}>
       <Paper className={style.signUpform} elevation={16}>
